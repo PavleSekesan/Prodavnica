f53d2918b076cefa1cfe14143b6021f2ec5d210d
PavleSekesan <48137570+PavleSekesan@users.noreply.github.com>
2021-06-05 15:57:33 +0200

    Promenjeno instanciranje prodavnica

---
+++ Prodavnica/Prodavnice/ProdavnicaFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodavnica.Prodavnice
+{
+    public static class ProdavnicaFactory
+    {
+        public static Prodavnica NovaProdavnica(double povrsina, string adresa)
+        {
+            if (povrsina <= 70)
+            {
+                return new MalaProdavnica(povrsina, adresa);
+            }
+            else if (povrsina > 70 && povrsina <= 200)
+            {
+                return new SrednjaProdavnica(povrsina, adresa);
+            }
+            else if (povrsina > 200 && povrsina <= 400)
+            {
+                return new VelikaProdavnica(povrsina, adresa);
+            }
+            else
+            {
+                return new Supermarket(povrsina, adresa);
+            }
+        }
+    }
+}
